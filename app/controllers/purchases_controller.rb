b6d4f84dbe00686ed7bf3d0061c0c5c4b83e1f84
John Athayde <john@hyphenateme.com>
2010-03-15 17:41:42 -0400

    updating purchases controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,15 +2,11 @@ class PurchasesController < ApplicationController
   def create
     @purchase = Purchase.new(params[:purchase])
 
-    respond_to do |format|
-      if @purchase.save
-        flash[:notice] = 'Thanks!'
-        format.html { redirect_to(@purchase) }
-        format.xml  { render :xml => @purchase, :status => :created, :location => @purchase }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @purchase.errors, :status => :unprocessable_entity }
-      end
+    if @purchase.save
+      flash[:notice] = 'Thanks!'
+    else
+      flash[:error] = 'That didn\'t work, yo'
     end
+    redirect_to(pages_path)
   end
 end
