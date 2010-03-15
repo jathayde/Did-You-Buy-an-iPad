122e9ad38a3f2800b329ec39d726dcd31231354b
John Athayde <john@hyphenateme.com>
2010-03-14 23:35:27 -0400

    working on page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,25 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.xml
   def index
+    @purchase = Purchase.new
 
+   end
+
+  # POST /pages
+  # POST /pages.xml
+  def create
+    @purchase = Purchase.new(params[:purchase])
+
+    respond_to do |format|
+      if @purchase.save
+        flash[:notice] = 'Thanks!'
+        format.html { redirect_to(@purchase) }
+        format.xml  { render :xml => @purchase, :status => :created, :location => @purchase }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @purchase.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :pages
+  map.resources :pages, :purchases
 
   # The priority is based upon order of creation: first created -> highest priority.
 
