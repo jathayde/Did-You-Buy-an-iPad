61b3f93901449727880fbfbdadf2f694d9ff62d5
Tom Copeland <tom@infoether.com>
2010-03-19 16:21:39 -0400

    Email notifications seem to be working, so adding Doug and John to the recipients list

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -3,7 +3,7 @@ class Notifications < ActionMailer::Base
 
   def new_purchase(purchase)
     subject    "[didyoubuyanipad] New purchase recorded (#{Purchase.count} so far)"
-    recipients %w{tom@infoether.com} # dougmarch@gmail.com, jathyade@infoether.com
+    recipients %w{tom@infoether.com dougmarch@gmail.com jathyade@infoether.com}
     from       "jathyade@boboroshi.com"
     sent_on    Time.now
     body       :purchase => purchase
